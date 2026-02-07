18565dc223cb00d032066c13cb7116c2c6e772b6
Vraj patel <patelvraj7372@gmail.com>
2026-02-07 14:36:35 +0530

    2Day

--- src/main/java/com/Grownited/controller/CategoryController.java
+++ src/main/java/com/Grownited/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package com.Grownited.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Grownited.entity.CategoryEntity;
 import com.Grownited.repository.CategoryRepository;
+
 
 //JPA -> specification  
 
@@ -21,7 +25,6 @@ public class CategoryController {
 		return "NewCategory";
 	}
 
-	
 	@PostMapping("saveCategory")
 	public String saveCategory(CategoryEntity categoryEntity) {
 
@@ -31,5 +34,14 @@ public class CategoryController {
 		return "AdminDashboard";
 	}
 	
+	@GetMapping("listCategory")
+	public String listCategory(Model model) {
+		
+		List<CategoryEntity> categoryList = categoryRepository.findAll();
+		model.addAttribute("categoryList",categoryList);//
+		
+		return "ListCategory";
+	}
+		
 
 }
