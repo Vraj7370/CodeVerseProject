859cb6bd0d88e7747909b7a5c8cc98cb21715d3d
Vraj patel <patelvraj7372@gmail.com>
2026-03-17 10:30:08 +0530

    commited

--- src/main/java/com/Grownited/filter/AuthFilter.java
+++ src/main/java/com/Grownited/filter/AuthFilter.java
@@ -20,38 +20,52 @@ import jakarta.servlet.http.HttpSession;
 @Component
 public class AuthFilter implements Filter {
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;
-		
-		String url = req.getRequestURL().toString();
-		String uri = req.getRequestURI().toString();
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
 
-		ArrayList<String> publicUrl = new ArrayList<>();
+        String uri = req.getRequestURI();
 
-		publicUrl.add("/login");
-		publicUrl.add("/signup");
-		publicUrl.add("/forget-password");
-		publicUrl.add("/authenticate");
+        ArrayList<String> publicUrl = new ArrayList<>();
 
-		if (publicUrl.contains(uri) || uri.contains("assets")) {
-			// go ahead
-			chain.doFilter(request, response);
-		} else {
-			System.out.println("AuthFilter ......" + new Date());
-			System.out.println(uri);
-			HttpSession session = req.getSession();
-			UserEntity user = (UserEntity) session.getAttribute("user");
-			if (user == null) {
-				res.sendRedirect("/login");
-			} else {
-				chain.doFilter(request, response);
-			}
+        // Public pages
+        publicUrl.add("/login");
+        publicUrl.add("/signup");
+        publicUrl.add("/register");
+        publicUrl.add("/authenticate");
+        publicUrl.add("/forgetpassword");
+        publicUrl.add("/forgot-password");
+        publicUrl.add("/reset-password");
 
-		}
+        // Static resources
+        if (publicUrl.contains(uri) || uri.contains("assets") || uri.contains("css") 
+                || uri.contains("js") || uri.contains("images")) {
 
-	}
+            chain.doFilter(request, response);
+        } 
+        else {
+
+            System.out.println("AuthFilter Running : " + new Date());
+            System.out.println("Requested URI : " + uri);
+
+            HttpSession session = req.getSession(false);
+
+            if (session == null) {
+                res.sendRedirect("/login");
+                return;
+            }
+
+            UserEntity user = (UserEntity) session.getAttribute("user");
+
+            if (user == null) {
+                res.sendRedirect("/login");
+            } 
+            else {
+                chain.doFilter(request, response);
+            }
+        }
+    }
 }
